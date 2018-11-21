2d77845a2c75a13d6f290b7717fb939b3e4f7317
Florian Sansou <fl.sansou@gmail.com>
2018-11-21 13:52:14 +0100

    Creation class voiture

---
+++ voiture.py
@@ -0,0 +1,9 @@
+class Voiture:
+    def __init__(self, masse, longueur, largeur):
+        self.masse = masse
+        self.longueur = longueur
+        self.largeur = largeur
+        self.acceleration = []
+        self.direction = []
+        self.position = []
+
