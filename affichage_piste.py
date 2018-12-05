1dd9aaebd3d885e5dcd1815d995af92e2cc05f3a
luscawalker <45207184+luscawalker@users.noreply.github.com>
2018-12-05 14:59:50 +0100

    Copie aéroport

--- affichage_piste.py
+++ affichage_piste.py
@@ -6,11 +6,12 @@ from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPen, QBrush, QColor, QPolygonF
 
 import piste
-
+LARGEUR = piste.LARGEUR
 WIDTH = 800  # Initial window width (pixels)
 HEIGHT = 450  # Initial window height (pixels)
 AIRPORT_Z_VALUE = 0
 TK_COLOR = "black"
+ANIMATION_DELAY = 50  # milliseconds
 
 
 class PanZoomView(QtWidgets.QGraphicsView):
@@ -51,6 +52,8 @@ class Dessin(QtWidgets.QWidget):
         self.scene.setBackgroundBrush(QColor('green'))
         self.view = PanZoomView(self.scene)
         self.time_entry = QtWidgets.QLineEdit()
+        toolbar = self.create_toolbar()
+        
         self.point = piste.creationpiste(600)
 
         # invert y axis for the view
@@ -62,10 +65,28 @@ class Dessin(QtWidgets.QWidget):
 
         # add components to the root_layout
         root_layout.addWidget(self.view)
+        root_layout.addLayout(toolbar)
 
         # show the window
         self.show()
 
+    def create_toolbar(self):
+        # create layout for time controls and entry
+        toolbar = QtWidgets.QHBoxLayout()
+
+        def add_button(text, slot):
+            """adds a button to the hbox and connects the slot"""
+            button = QtWidgets.QPushButton(text)
+            button.clicked.connect(slot)
+            toolbar.addWidget(button)
+
+        # lambda function allows to pass extra arguments to slots
+        # added space around '-' character to avoid different look and feel
+        add_button(' - ', lambda: self.view.zoom_view(0.9))
+        add_button('+', lambda: self.view.zoom_view(1.1))
+        toolbar.addStretch()
+        add_button('|>', self.playpause)
+        toolbar.addStretch()
 
     def add_piste(self):
 
@@ -73,7 +94,7 @@ class Dessin(QtWidgets.QWidget):
         self.scene.addItem(track_group)
 
 
-        pen = QPen(QtGui.QColor(TK_COLOR), 15)
+        pen = QPen(QtGui.QColor(TK_COLOR), LARGEUR)
         pen.setCapStyle(QtCore.Qt.RoundCap)
         
         path = QtGui.QPainterPath()
@@ -81,7 +102,17 @@ class Dessin(QtWidgets.QWidget):
         path.moveTo(point[0].x, point[0].y)
         
         self.scene.addRect(point[0].x,point[0].y-10,40,20,QPen(QtGui.QColor(TK_COLOR),1),QBrush(QColor('black')))
+        
         for i in range(1, len(point)):
             path.lineTo(point[i].x, point[i].y)
         item = QtWidgets.QGraphicsPathItem(path, track_group)
         item.setPen(pen)
+        
+        
+    @QtCore.pyqtSlot()
+    def playpause(self):
+        """this slot toggles the replay using the timer as model"""
+        if self.timer.isActive():
+            self.timer.stop()
+        else:
+            self.timer.start(ANIMATION_DELAY)
