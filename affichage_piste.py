3edb2684c68acfe426b7c2e765276e82d7c7bd10
luscawalker <45207184+luscawalker@users.noreply.github.com>
2019-01-14 11:11:14 +0100

    Merge branch 'master' of C:\Users\Lucas\Documents\ENAC\Git with conflicts.

--- affichage_piste.py
+++ affichage_piste.py
@@ -70,7 +70,7 @@ class Dessin(QtWidgets.QWidget):
                 mon_depickler=pickle.Unpickler(fichier)
                 self.chemin  = mon_depickler.load()
                 self.lancerastar()
-                
+
         elif choice == 3:
             with open('alldata','rb') as fichier:
                 depickler = pickle.Unpickler(fichier)
@@ -82,14 +82,15 @@ class Dessin(QtWidgets.QWidget):
             self.ex = mouse_tracker.MouseTracker()
             self.ex.listeMouseTracker.connect(self.listemousetracker)
             self.ex.setWindowModality(QtCore.Qt.ApplicationModal)
-            self.ex.showMaximized()
-    
+            self.ex.show()
+
     def defvoiture(self):
-        [self.car.name,self.car.vitessemax,self.car.accelerationmax,pasvirage] = self.firstview.choisie
-        self.car.pasvirage = int(pasvirage)*np.pi/(180*self.car.DELTAVIR)
+        [self.car.name,self.car.vitessemax,accelerationmax,virage,self.car.empattement,self.car.masse, self.car.longueur, self.car.largeur] = self.firstview.choisie
+        self.car.pasvirage = float(virage)*np.pi/(180*self.car.deltavir)
+        self.car.pasacceleration = float(accelerationmax)/self.car.deltaacc
         self.voiturechoisie = True
         self.ready =True
-        
+
     def lancerastar(self):
         self.firstview = presentationvoiture.FirstView()
         self.firstview.voiturechoisie.connect(self.defvoiture)
@@ -160,14 +161,11 @@ class Dessin(QtWidgets.QWidget):
         pen = QPen(QtGui.QColor(TK_COLOR), LARGEUR)
         pen.setJoinStyle(Qt.RoundJoin)
         self.extrem(4)
-        
+
         path = QtGui.QPainterPath()
         path.moveTo(self.piste[0].x, self.piste[0].y)
         for i in range(1, len(self.piste)):
             path.lineTo(self.piste[i].x, self.piste[i].y)
-            #self.scene.addLine(self.chemin[0][i - 1].x, self.chemin[0][i - 1].y, self.chemin[0][i].x,self.chemin[0][i].y, QPen(QtGui.QColor(255, 0, 0), 0)) #affiche le centre de la piste
-            #self.scene.addLine(self.chemin[1][i-1].x, self.chemin[1][i-1].y,self.chemin[1][i].x,self.chemin[1][i].y, QPen(QtGui.QColor(255,0,0), 0)) #affiche le cote gauche de la piste
-            #self.scene.addLine(self.chemin[2][i - 1].x, self.chemin[2][i - 1].y, self.chemin[2][i].x,self.chemin[2][i].y, QPen(QtGui.QColor(255, 0, 0), 0)) #affiche le cote droit de la piste
         #self.scene.addPolygon(Polygone(self.chemin[1][-1],self.chemin[2][-1],self.piste[-1],self.piste[-5], 20), QPen(QtGui.QColor(TK_COLOR), 1), QBrush(QColor(TK_COLOR)))
         item = QtWidgets.QGraphicsPathItem(path, track_group)
         item.setPen(pen)
@@ -240,7 +238,6 @@ class Dessin(QtWidgets.QWidget):
     def miseajour(self):
         if self.voiturechoisie:
             astar2.astar(self.chemin, self.car)
-            print("A* fini")
             self.voiturechoisie = False
             self.mainwindows()
         if self.ready:
@@ -251,31 +248,3 @@ class Dessin(QtWidgets.QWidget):
             picker = pickle.Pickler(fichier)
             picker.dump([self.chemin,self.car])
         print('Sauvegarde réussie')
-
-'''def Polygone(A, B, longueur):
-    theta = math.atan((B.y - A.y) / (B.x - A.x))
-    B1 = QPoint(B.x - LARGEUR * math.sin(theta) / 2, B.y + LARGEUR * math.cos(theta) / 2)
-    B2 = QPoint(B.x + LARGEUR * math.sin(theta) / 2, B.y - LARGEUR * math.cos(theta) / 2)
-    C = piste.Point(B.x + longueur * (B.x - A.x), B.y + longueur * (B.y - A.y))
-    C1 = QPoint(C.x - LARGEUR * math.sin(theta) / 2, C.y + LARGEUR * math.cos(theta) / 2)
-    C2 = QPoint(C.x + LARGEUR * math.sin(theta) / 2, C.y - LARGEUR * math.cos(theta) / 2)
-    Poly = QPolygonF()
-    lt = [B1, B2, C2, C1]
-    for i in range(len(lt)):
-        Poly.append(lt[i])
-    return (Poly)
-    
-def Polygone(A,B,C,D,longueur):
-    deltax,deltay=C.x-D.x,C.y-D.y
-    v=math.sqrt(deltax**2+deltay**2)
-    if v!=0:
-        deltax,deltay=deltax/v,deltay/v
-    P1=QPoint(A.x,A.y)
-    P2=QPoint(A.x+longueur*deltax,A.y+longueur*deltay)
-    P3=QPoint(B.x+longueur*deltax,B.y+longueur*deltay)
-    P4=QPoint(B.x,B.y)
-    Poly = QPolygonF()
-    lt = [P1,P2,P3,P4]
-    for i in range(4):
-        Poly.append(lt[i])
-    return(Poly)'''
